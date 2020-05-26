34773fd0aac271d1b7a0b486d85facada24a9e9d
IlyaUsmanov <60606315+IlyaUsmanov@users.noreply.github.com>
2020-05-26 23:20:32 +0300

    Update tools.py

--- tools.py
+++ tools.py
@@ -3,25 +3,25 @@ import string
 
 def get_alphabet():
     alphabet = []
-    for i in string.ascii_letters:
-        alphabet.append(i)
-    for i in string.punctuation:
-        alphabet.append(i)
+    for letter in string.ascii_letters:
+        alphabet.append(letter)
+    for letter in string.punctuation:
+        alphabet.append(letter)
     return alphabet
 
 
 def calc_difference(freq1, freq2):
     alphabet = get_alphabet()
     dif = 0
-    for i in alphabet:
-        dif += (freq1[i] - freq2[i]) ** 2
+    for letter in alphabet:
+        dif += (freq1[letter] - freq2[letter]) ** 2
     return dif
 
 
 def calc_frequency(text):
     alphabet = get_alphabet()
-    amount = sum(text.count(i) for i in alphabet)
+    amount = sum(text.count(letter) for letter in alphabet)
     frequency = {}
-    for i in alphabet:
-        frequency[i] = text.count(i) / amount
+    for letter in alphabet:
+        frequency[letter] = text.count(letter) / amount
     return frequency
