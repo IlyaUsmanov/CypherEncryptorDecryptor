bf66fc09e4e0a4418a131d52e6cad142317ab7e4
IlyaUsmanov <60606315+IlyaUsmanov@users.noreply.github.com>
2020-05-26 23:19:40 +0300

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -23,13 +23,13 @@ class Caesar:
 
     def hack(text, frequency):
         alphabet = tools.get_alphabet()
-        min_d = len(text)
+        min_difference = len(text)
         ans_shift = -1
         for shift in range(len(alphabet)):
             newtext = Caesar.encode(text, shift)
             temp_freq = tools.calc_frequency(newtext)
-            d = tools.calc_difference(frequency, temp_freq)
-            if d <= min_d:
-                min_d = d
+            difference = tools.calc_difference(frequency, temp_freq)
+            if difference <= min_difference:
+                min_difference = difference
                 ans_shift = shift
         return Caesar.encode(text, ans_shift)
